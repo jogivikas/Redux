edb623678416412983c17e1900e987f1eceb2802
vikas jogi <jogivikas666@gmail.com>
2025-09-18 22:45:26 +0530

    add provider and useselector

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
+import Todo from './components/Todo'
+import { store } from './app/store';
+
+import {Provider} from"react-redux";
+
 
 function App() {
   
 
   return (
     <>
-      
+     <Provider store={store}>
+     <Todo/>
+     </Provider>
     </>
   )
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,20 @@
+
+import { useSelector } from "react-redux"
+export default function Todo()
+{
+   const todos= useSelector((state) => state.todo.todos);
+   console.log(todos);
+    return(
+
+        <>
+        <h2>Todo List</h2>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>
+                    {todo.task} - {todo.isDone ? "Done" : "Not Done"}
+                </li>
+            ))}
+        </ul>
+        </>
+    )
+}
